2eb81720db124927b4cac3dd249665ccb7d6a38f
Rubens Oliveira <rubensocj@gmail.com>
2022-08-04 11:16:17 -0300

    Add files via upload

---
+++ table.summary.aov.R
@@ -0,0 +1,41 @@
+#' Title
+#'
+#' @param model 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+table.summary.aov <- function(model) {
+  suppressWarnings(if(is.na(match(class(model), c("aov")))) {
+    stop("Invalid argument type: 'model' must be of class: 'aov'")
+  })
+  
+  # results of ANOVA
+  res <- rbind.data.frame(summary.aov(mod.aov))
+  colnames(res) <- c('Df','Sum Sq','Mean Sq','F value','Pr(>F)')
+  
+  # source of variation
+  src <- c(attributes(mod.aov$terms)$term.labels, "Residuals")
+  tb <- cbind("Source of variation" = src, res)
+  
+  # signif. codes
+  sig.cod <- c('***','**','*','.',' ')
+  sig <- rep('', nrow(tb))
+  sig[res$`Pr(>F)` >= 0 & res$`Pr(>F)` <= 0.001] <- sig.cod[1]
+  sig[res$`Pr(>F)` > 0.001 & res$`Pr(>F)` <= 0.01] <- sig.cod[2]
+  sig[res$`Pr(>F)` > 0.01 & res$`Pr(>F)` <= 0.05] <- sig.cod[3]
+  sig[res$`Pr(>F)` > 0.05 & res$`Pr(>F)` <= 0.1] <- sig.cod[4]
+  sig[res$`Pr(>F)` > 0.1 & res$`Pr(>F)` <= 1] <- sig.cod[5]
+  
+  # format
+  tb[ , 3:5] <- round(tb[ , 3:5], 2)
+  tb$`F value` <- as.character(tb$`F value`)
+  tb$`F value`[is.na(tb$`F value`)] <- ''
+  tb$`Pr(>F)` <- format(tb$`Pr(>F)`, scientific = T, digits = 3)
+  tb$`Pr(>F)` <- as.character(tb$`Pr(>F)`)
+  tb$`Pr(>F)` <- gsub('NA','',tb$`Pr(>F)`)
+  tb$`Pr(>F)` <- paste0(tb$`Pr(>F)`, ' ', sig)
+  
+  return(tb)
+}
